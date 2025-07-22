cf73fea36bd6c6be4ace9bc91f855f07c5a7a26e
bandrada <bandrada.dev@gmail.com>
2025-07-22 16:19:00 -0400

    add questions. weird behavior noticed

--- createuser.sh
+++ createuser.sh
@@ -2,14 +2,18 @@
 
 pacman -S --noconfirm gum
 
+echo "What would you like your usename to be?"
 USER=$(gum input --placeholder "username" --prompt "Username> ")
+
 useradd -m $USER
 passwd $USER
 echo "$USER   ALL=(ALL:ALL) ALL" >> /etc/sudoers
 echo "[user]" >> /etc/wsl.conf
 echo "default=$USER" >> /etc/wsl.conf
 
+echo "What would you like your hostname to be?"
 HOST=$(gum input --placeholder "hostname" --prompt "Hostname> ")
+
 echo "[network]" >> /etc/wsl.conf
 echo "hostname=$HOST" >> /etc/wsl.conf
 echo "generateHosts=false" >> /etc/wsl.conf
